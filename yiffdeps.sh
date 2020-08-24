ae3d23415337305afb405d221c1d9593fe786db7
Fox RunTime <white._tail@icloud.com>
2020-08-24 21:42:56 +0100

    genericify
    name change all the things

--- yiffdeps.sh
+++ yiffdeps.sh
@@ -1,6 +1,6 @@
-mkdir ~/Desktop/yiffselector
-echo "directory made on your desktop called yiffselector"
-cd ~/Desktop/yiffselector
+mkdir ~/Desktop/rfile
+echo "directory made on your desktop called rfile"
+cd ~/Desktop/rfile
 echo "we are now in that directory:"
 pwd
 curl -f https://raw.githubusercontent.com/FoxRunTime/rfile/master/impbcopy.m -o impbcopy.m
@@ -12,12 +12,12 @@ echo "special sauce now authorised for execution"
 rm -r impbcopy.dSYM
 rm impbcopy.m
 echo "trashed regular sauce"
-curl -f https://raw.githubusercontent.com/FoxRunTime/rfile/master/rfile.sh -o yiff.sh
+curl -f https://raw.githubusercontent.com/FoxRunTime/rfile/master/rfile.sh -o rfile.sh
 echo "this is what you're after"
-chmod +x yiff.sh
+chmod +x rfile.sh
 echo "authorised for execution"
-./yiff.sh
-echo "if all has gone to plan, you'll now see a random file from the directory in yiff.sh and it'll be on your pasteboard too"
-echo "run ./yiff.sh in ~/Desktop/yiffselector to do it again"
+./rfile.sh
+echo "if all has gone to plan, you'll now see a random file from the directory in rfile.sh and it'll be on your pasteboard too"
+echo "run ./rfile.sh in ~/Desktop/rfile to do it again"
 
 echo "by foxruntime"
