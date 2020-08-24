b889340bd7cfb684c4fc1b7d301234e4378648e4
Fox RunTime <white._tail@icloud.com>
2020-08-24 22:09:39 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ echo "we are now in that directory:"
 pwd
 curl -f https://raw.githubusercontent.com/FoxRunTime/rfile/master/impbcopy.m -o impbcopy.m
 echo "sauce obtained"
-gcc -Wall -g -O3 -ObjC -framework Foundation -framework AppKit -o impbcopy ~/Desktop/yiffselector/impbcopy.m
+gcc -Wall -g -O3 -ObjC -framework Foundation -framework AppKit -o impbcopy ~/Desktop/rfile/impbcopy.m
 echo "sauce compiled into special sauce"
 chmod +x impbcopy
 echo "special sauce now authorised for execution"
